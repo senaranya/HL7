82110248758011ebcb35861eccabc9e37cfc203f
Leo Lutz <leo@leolutz.com>
2025-01-29 20:31:36 -0700

    Add test for withSegmentEndingFieldSeparator(true/false) and improve default test

--- tests/HL7Test.php
+++ tests/HL7Test.php
@@ -77,9 +77,29 @@ class HL7Test extends TestCase
     public function withSegmentEndingFieldSeparator_defaults_to_true(): void
     {
         $msg = HL7::build()
+            ->withHL7Version('555.666')
             ->withSegmentEndingFieldSeparator()
             ->createMessage();
-        self::assertStringEndsWith('|\n', $msg->toString());
+        self::assertStringEndsWith('555.666|\n', $msg->toString());
+    }
+
+    /**
+     * @test
+     * @throws HL7Exception
+     */
+    public function withSegmentEndingFieldSeparator_works_with_values(): void
+    {
+        $msg = HL7::build()
+            ->withHL7Version('555.666')
+            ->withSegmentEndingFieldSeparator(true)
+            ->createMessage();
+        self::assertStringEndsWith('555.666|\n', $msg->toString());
+
+        $msg = HL7::build()
+            ->withHL7Version('555.666')
+            ->withSegmentEndingFieldSeparator(false)
+            ->createMessage();
+        self::assertStringEndsWith('555.666\n', $msg->toString());
     }
 
     /**
